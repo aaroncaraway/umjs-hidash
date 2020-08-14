61d53f777dbed1f785647d703ea2a79abf6c46ba
aaroncaraway <aaroncaraway42@gmail.com>
2020-08-14 12:57:31 -0700

    S32 446 complete

--- index.text.js
+++ index.text.js
@@ -1,27 +1,40 @@
 const { forEach, map } = require("./index");
 
-let sum = 0;
-forEach([1, 2, 3], (value) => {
-  sum += value;
+const test = (description, fn) => {
+  console.log("----", description);
+  try {
+    fn();
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
+test("The forEach test", () => {
+  let sum = 0;
+  forEach([1, 2, 3], (value) => {
+    sum += value;
+  });
+
+  if (sum !== 7) {
+    throw new Error("Expected sum to equal 6");
+  }
 });
 
-if (sum !== 6) {
-  throw new Error("Expected sum to equal 6");
-}
+test("The map test", () => {
+  let results = [];
+  map([1, 2, 3], (value) => {
+    results.push(value * 2);
+  });
 
-let results = [];
-map([1, 2, 3], (value) => {
-  results.push(value * 2);
+  if (results[0] !== 2) {
+    throw new Error("nope!");
+  }
+
+  if (results[1] !== 4) {
+    throw new Error("nope!");
+  }
+
+  if (results[2] !== 6) {
+    throw new Error("nope!");
+  }
 });
-
-if (results[0] !== 2) {
-  throw new Error("nope!");
-}
-
-if (results[1] !== 4) {
-  throw new Error("nope!");
-}
-
-if (results[2] !== 6) {
-  throw new Error("nope!");
-}
